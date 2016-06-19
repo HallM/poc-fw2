88128483cf12376fd5ab57edfb0a9578ff0fc8cf
Matthew Hall <mdhall@gmail.com>
2016-06-18 21:40:38 -0500

    removing built JS files

--- framework/errors/notfound.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const index_1 = require('./index');
-class NotFoundError extends index_1.HttpError {
-    constructor(message) {
-        super(message, 404);
-    }
-}
-exports.NotFoundError = NotFoundError;
