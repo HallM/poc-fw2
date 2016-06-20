bea0cb312c598c37c6f6f133c1afc8da0d54c9f5
Matthew Hall <mhall@bouncingpixel.com>
2016-06-20 13:52:55 -0500

    proof inject works in components, outside of pages

--- example/server/components/test.ts
+++ example/server/components/test.ts
@@ -1,5 +1,12 @@
-export = class TestComponent {
+import { InjectService } from '../../../service-manager/';
+
+class TestComponent {
+  @InjectService('req')
+  req: any
+
   test() {
-    return 'something';
+    return 'inside component, req.protocol: ' + this.req.protocol;
   }
 }
+
+export = TestComponent;
