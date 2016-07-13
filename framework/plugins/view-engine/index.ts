318f73d10ce76e46a2e0170279b49773ea1b931b
Matthew Hall <mhall@bouncingpixel.com>
2016-07-13 17:42:27 -0500

    wip: injections

--- framework/plugins/view-engine/index.ts
+++ framework/plugins/view-engine/index.ts
@@ -9,8 +9,13 @@ import * as express from 'express';
 
 import { serviceManager } from '../../';
 import { InjectServiceMetaKey } from '../../../service-manager/';
+import { ReqParamMetaKey } from '../../decorators/req-param';
 
 var expressLisplate = require('express-lisplate');
+
+interface ViewModelInterface {
+  new(data: any): ViewModelInterface
+}
 
 export default class ViewEngine {
     static pluginName: string = 'view-engine'
@@ -29,11 +34,24 @@ export default class ViewEngine {
                 var modelpath = path.resolve(templateName + '.js');
                 var viewmodel = null;
                 try {
-                    viewmodel = require(modelpath);
+                    var vm: ViewModelInterface = require(modelpath);
+
+                    viewmodel = class ViewModel extends vm {
+                        constructor(data) {
+                            super(data);
+                            if (data && data.$_svc) {
+                                var svc = data.$_svc;
+
+                                const needinjects: string[] = Reflect.getMetadata(InjectServiceMetaKey, vm) || [];
+
+                                // Now do injections
+                                // but... components wind up not having access to these
+                                // is that a problem, or not? are pages the only thing allowed to have these?
+                            }
+                        }
+                    };
                 } catch(e) {
                 }
-
-                // TODO: do injections
 
                 return viewmodel;
             },
@@ -45,7 +63,7 @@ export default class ViewEngine {
 
         app.use(expressLisplate.localizationInit);
 
-        // app.use(function(req: express.Request, res: any, next: express.NextFunction) {
+        app.use(function(req: express.Request, res: any, next: express.NextFunction) {
         //     res.streamPage = function(page) {
         //         const svc = serviceManager.makeRequestContext();
         //         svc.addService('req', req);
@@ -57,7 +75,8 @@ export default class ViewEngine {
         //         dust.stream(page, localstack).pipe(res);
         //     };
 
-        //     next();
-        // });
+            res.locals.$_svc = serviceManager.makeRequestContext();
+            next();
+        });
     }
 }
